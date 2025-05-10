f79b0688bcd2cd8899d6b93c359d977277250da5
luluwaffless <lcsgoes210@gmail.com>
2025-05-10 01:34:06 -0300

    1.1.0

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 // catbox-cli by luluwaffless
 import { readFileSync, writeFileSync, statSync, createReadStream } from "node:fs";
+import { createInterface } from "node:readline/promises";
 import { basename, normalize } from "node:path";
 import FormData from "form-data";
+import chalk from "chalk";
 import got from "got";
-import { createInterface } from "node:readline/promises";
 
 // option parser
 const rl = createInterface({ input: process.stdin, output: process.stdout });
@@ -33,17 +34,24 @@ if (argFlags.length > 1) {
     process.exit(1);
 };
 
+// colors
+const blue = chalk.hex("#333EBD");
+const lightBlue = chalk.hex("#5BCEFA");
+const purple = chalk.hex("#BE18D6");
+const pink = chalk.hex("#FF75A2");
+const lightPink = chalk.hex("#F5A9B8");
+
 // code begin
 (async () => {
     const defaultHash = readFileSync(".userhash", "utf8");
     const validTimes = ['1h', '12h', '24h', '72h'];
     if (options.help) { // help message
-        console.log(`Usage: catbox ./path/to/file [OPTION]
-Uploads the file permanently to Catbox and returns the URL. (200MB file size limit)
-Options:
-    --help: Shows this message.
-    --userhash <hash>: Uses a specific userhash for Catbox.
-    --time <time>: Uploads the file temporarily to Litterbox, valid time options are "${validTimes.join('", "')}". (1GB file size limit)`);
+        console.log(`${lightBlue("Usage:")} ${lightPink("catbox")} ./path/to/file ${blue("[OPTION]")}
+${pink("Uploads the file to Catbox and returns the URL.")}
+${lightPink("Options:")}
+    --help: ${pink("Show usage information.")}
+    --userhash ${purple("<hash>")}: ${pink("Use a specific Catbox userhash for uploads (prompts to save as default if not set).")}
+    --time ${purple("<time>")}: ${pink(`Upload to Litterbox (temporary), valid times: "${validTimes.join('", "')}".`)}`);
         process.exit(0);
     } else if (options.filePath) { // file upload
         const filePath = normalize(options.filePath);
@@ -52,10 +60,10 @@ Options:
             stats = statSync(filePath);
         } catch (err) {
             if (err.code === 'ENOENT') {
-                console.error('File does not exist.');
+                console.error(purple('File does not exist.'));
                 process.exit(1);
             } else {
-                console.error('Error checking file:', err);
+                console.error(purple('Error checking file:'), err);
                 process.exit(1);
             };
         };
@@ -63,7 +71,7 @@ Options:
             if (validTimes.includes(options.time)) {
                 try { // uploading
                     if (stats.size > 1073741824) {
-                        console.error("ERROR: File size exceeds the 1GB limit for Litterbox. You will need to use another service.");
+                        console.error(purple("ERROR: File size exceeds the 1GB limit for Litterbox. You will need to use another service."));
                         process.exit(1);
                     };
 
@@ -81,43 +89,43 @@ Options:
                     let uploaded = 0;
                     form.on("data", chunk => {
                         uploaded += chunk.length;
-                        process.stdout.write(`\rUploading "${fileName}" to Litterbox for ${options.time}... (${((uploaded / totalLength) * 100).toFixed(2)}%)`);
+                        process.stdout.write(`\rUploading ${pink(`"${fileName}"`)} to ${lightBlue("Litterbox")} for ${options.time}... ${purple(`(${((uploaded / totalLength) * 100).toFixed(2)}%)`)}`);
                     });
                     const response = await got.post("https://litterbox.catbox.moe/resources/internals/api.php", {
                         body: form,
                         headers: form.getHeaders(),
                     }).text();
-                    console.log(`\nUploaded "${fileName}" successfully! URL: ${response}`);
+                    console.log(`\nUploaded ${pink(`"${fileName}"`)} successfully! URL: ${blue(response)}`);
                     process.exit(0);
                 } catch (err) {
-                    console.error("\nError uploading file:", err);
+                    console.error(purple("\nError uploading file:"), err);
                     process.exit(1);
                 }
             } else {
-                console.error(`ERROR: Invalid time option. Use --help for usage example.`);
+                console.error(purple(`ERROR: Invalid time option. Use --help for usage example.`));
                 process.exit(1);
             };
         } else { // catbox
             const userhash = options.userhash || defaultHash; // use the userhash from the command line or the default one
             if (userhash && options.userhash && !defaultHash) { // prompt to save userhash if not already saved
-                const saveHash = await rl.question(`No default userhash. Would you like to set the inputted userhash "${userhash}" as default for future uploads? (y/n) `);
+                const saveHash = await rl.question(purple(`No default userhash. Would you like to set the inputted userhash "${userhash}" as default for future uploads? (y/n) `));
                 if (saveHash.toLowerCase().startsWith("y")) {
                     writeFileSync(".userhash", userhash, "utf8");
-                    console.log(`Userhash "${userhash}" saved as default.`);
-                } else console.log(`Userhash "${userhash}" not saved.`);
+                    console.log(purple(`Userhash "${userhash}" saved as default.`));
+                } else console.log(purple(`Userhash "${userhash}" not saved.`));
             } else if (!userhash) { // prompt to upload anonymously or cancel
-                const uploadAnyways = await rl.question(`No userhash inputted. Would you like to upload anyways? (y/n) `);
+                const uploadAnyways = await rl.question(purple(`No userhash inputted. Would you like to upload anyways? (y/n) `));
                 if (uploadAnyways.toLowerCase().startsWith("y")) {
-                    console.log(`Uploading anonymously.`);
+                    console.log(purple(`Uploading anonymously.`));
                 } else {
-                    console.log(`Upload cancelled. You may set a default userhash by uploading again using --userhash followed by your userhash.`);
+                    console.log(purple(`Upload cancelled. You may set a default userhash by uploading again using --userhash followed by your userhash.`));
                     process.exit(0);
                 };
             };
 
             try { // uploading
                 if (stats.size > 209715200) {
-                    console.error("ERROR: File size exceeds the 200MB limit for Catbox. Try using Litterbox instead (although temporary), check --help for details.");
+                    console.error(purple("ERROR: File size exceeds the 200MB limit for Catbox. Try using Litterbox instead (although temporary), check --help for details."));
                     process.exit(1);
                 };
                 const fileName = basename(filePath);
@@ -134,21 +142,21 @@ Options:
                 let uploaded = 0;
                 form.on("data", chunk => {
                     uploaded += chunk.length;
-                    process.stdout.write(`\rUploading "${fileName}" to Catbox... (${((uploaded / totalLength) * 100).toFixed(2)}%)`);
+                    process.stdout.write(`\rUploading ${pink(`"${fileName}"`)} to ${lightPink("Catbox")}... ${purple(`(${((uploaded / totalLength) * 100).toFixed(2)}%)`)}`);
                 });
                 const response = await got.post("https://catbox.moe/user/api.php", {
                     body: form,
                     headers: form.getHeaders(),
                 }).text();
-                console.log(`\nUploaded "${fileName}" successfully! URL: ${response}`);
+                console.log(`\nUploaded ${pink(`"${fileName}"`)} successfully! URL: ${blue(response)}`);
                 process.exit(0);
             } catch (err) {
-                console.error("\nError uploading file:", err);
+                console.error(purple("\nError uploading file:"), err);
                 process.exit(1);
             };
         };
     } else {
-        console.error(`ERROR: No file path specified. Use --help for usage example.`);
+        console.error(purple(`ERROR: No file path specified. Use --help for usage example.`));
         process.exit(1);
     };
 })();
